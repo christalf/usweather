d5619c8aba2654e2915ffe7331ae87fa69af9021
chris <karanicol@gmail.com>
2023-07-08 18:08:21 -0300

    Initial commit of new project

---
+++ lib/usweather.ex
@@ -0,0 +1,18 @@
+defmodule Usweather do
+  @moduledoc """
+  Documentation for `Usweather`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Usweather.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/usweather_test.exs
@@ -0,0 +1,8 @@
+defmodule UsweatherTest do
+  use ExUnit.Case
+  doctest Usweather
+
+  test "greets the world" do
+    assert Usweather.hello() == :world
+  end
+end
